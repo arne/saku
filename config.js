ab691d279602d3bff88f08a54584fab454315bb7
Marcus Ramberg <marcus.ramberg@gmail.com>
2018-10-06 13:14:19 +0200

    Add auth support for /create

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ let CONFIG = {} //Make this global to use all over the application
 CONFIG.name           = process.env.NAME            || 'Default Name'
 CONFIG.email          = process.env.EMAIL           || 'mail@example.com'
 CONFIG.site           = process.env.SITE            || 'https://example.com'
+CONFIG.password       = process.env.PASSWORD        || 'saku'
 
 CONFIG.app            = process.env.APP             || 'dev'
 CONFIG.port           = process.env.PORT            || '3000'
