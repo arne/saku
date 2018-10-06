6e9e1b091a0e5c576b234000ae386c90976ee796
Arne Fismen <arnefismen@gmail.com>
2018-10-06 22:45:26 +0200

    Using name as title-tag

--- controller/blog.js
+++ controller/blog.js
@@ -1,10 +1,11 @@
 const db = require('../db')
-
+const settings = db.settings.get()
+console.log(settings.name)
 const ctr = {
   index: function(ctx) {
     return ctx.render('index', {
-      title: 'hello',
-      settings: db.settings.get(),
+      title: settings.name,
+      settings: settings,
       posts: db.all()
     })
   }
