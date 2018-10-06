08201902579633b9a78fd6cdc0b7938c4068c0c5
Arne Fismen <arnefismen@gmail.com>
2018-10-06 12:40:19 +0200

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = new Router()
 const ctr = require('../controller')
 
 router.get(     '/',              async ctx => ctr.index(ctx))
+router.get(     '/create',        async ctx => ctr.create(ctx))
+router.post(    '/new',           async ctx => ctr.new(ctx))
 
 module.exports = router
